b96fb84feb4623f08d72569da0da488b12bb5d16
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-24 11:11:15 -0700

    add todo gql

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useQuery} from '@apollo/react-hooks';
+import {useQuery, useMutation} from '@apollo/react-hooks';
 import {gql} from 'apollo-boost';
 
 const GET_TODOS = gql`
@@ -14,8 +14,32 @@ const GET_TODOS = gql`
   }
 `;
 
+const ADD_TODO = gql`
+  mutation AddTodo($text: String!) {
+    createTodo(data: {text: $text, completed: false}) {
+      _id
+      text
+      completed
+    }
+  }
+`;
+
 function App() {
   const {loading, error, data} = useQuery(GET_TODOS);
+  const [addTodo] = useMutation(ADD_TODO, {
+    update(
+      cache,
+      {
+        data: {addTodo},
+      },
+    ) {
+      const {todos} = cache.readQuery({query: GET_TODOS});
+      cache.writeQuery({
+        query: GET_TODOS,
+        data: {todos: todos.concat([addTodo])},
+      });
+    },
+  });
 
   if (loading) return <div>Loading...</div>;
 
