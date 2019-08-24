656d5e6d8c3565b84e298486110cdcca491e8cb3
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-24 10:24:55 -0700

    polloclient

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import ApolloClient from 'apollo-boost';
+
+const client = new ApolloClient({
+  uri: `${process.env.DB_URI}`,
+  headers: {
+    authorization: `Bearer ${process.env.DB_BEARER_TOKEN}`,
+  },
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
